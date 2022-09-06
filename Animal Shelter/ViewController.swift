938da373cf53f9745709b93e10bae6ddebe7fc24
MaximLaskin <maxlaskin@mail.ru>
2022-09-06 17:06:04 +0300

    Initial Commit

---
+++ Animal Shelter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animal Shelter
+//
+//  Created by Swift on 06.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
