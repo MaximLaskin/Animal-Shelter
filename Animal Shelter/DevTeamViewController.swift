23e7c8262c6812bf7773643c58147d6447adc447
Pavel Shirokiy <pshirokiy@gmail.com>
2022-09-07 20:25:40 +0300

    new storyboard

---
+++ Animal Shelter/DevTeamViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DevTeamViewController.swift
+//  Animal Shelter
+//
+//  Created by 19543442 on 07.09.2022.
+//
+
+import UIKit
+
+class DevTeamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
