8f2b724b50be53b9d7f95b70d7435ce3d9755640
MaximLaskin <maxlaskin@mail.ru>
2022-09-07 18:31:35 +0300

    Добавил и настроил AnimalDetailsViewController

---
+++ Animal Shelter/AnimalDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AnimalDetailsViewController.swift
+//  Animal Shelter
+//
+//  Created by Swift on 07.09.2022.
+//
+
+import UIKit
+
+final class AnimalDetailsViewController: UIViewController {
+
+// MARK: - IBOutlets
+
+    @IBOutlet weak var nameAndBreedLabel: UILabel!
+
+    @IBOutlet weak var sexLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    @IBOutlet weak var shelterButton: UIButton!
+
+// MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
