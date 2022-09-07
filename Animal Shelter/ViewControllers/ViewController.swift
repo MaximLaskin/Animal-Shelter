2c4a40ba9d1a21c34c06450c913a7352cac957e9
Leo Fridman <madfridman@gmail.com>
2022-09-07 11:24:09 +0200

    Revert "Merge pull request #1 from MaximLaskin/Pavel"
    This reverts commit 8cd8723a042cb48d0898f27077a403cdfb43139a, reversing
    changes made to 6cd64b5560806abd03baa6a7e11040123f9e5476.

--- Animal Shelter/ViewControllers/ViewController.swift
+++ Animal Shelter/ViewControllers/ViewController.swift
@@ -9,25 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
- 
-    
-    
-    
-    
     override func viewDidLoad() {
-           super.viewDidLoad()
-           // Do any additional setup after loading the view.
-       }
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
 
-    
-    @IBAction func catsButton() {
-    }
-    
-    
-    @IBAction func dogsButton() {
-    }
-    
-   
 
 }
 
