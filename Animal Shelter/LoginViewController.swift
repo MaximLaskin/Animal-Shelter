6658dca2e0a571fff1daa0b40a5221a32423fe23
Leo Fridman <madfridman@gmail.com>
2022-09-07 17:36:39 +0200

    added LoginViewController and alerts settings

---
+++ Animal Shelter/LoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.swift
+//  Animal Shelter
+//
+//  Created by Leon Fridman on 9/7/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+// MARK: - AlertController
+//
+//extension LoginViewController {
+//    private func showAlert(with title: String, and massage: String) {
+//        let alert = UIAlertController(
+//            title: title,
+//            message: massage,
+//            preferredStyle: .alert)
+//
+//        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+//            self.passwordTextField.text = ""
+//        }
+//
+//        alert.addAction(okAction)
+//        present(alert, animated: true)
+//    }
+//}
