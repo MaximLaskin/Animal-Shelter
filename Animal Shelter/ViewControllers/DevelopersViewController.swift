7069d5979261e1f1c441cced0ab8ef1901a235e7
Leo Fridman <madfridman@gmail.com>
2022-09-09 13:20:47 +0200

    Constrains, Devs ViewConroller added

---
+++ Animal Shelter/ViewControllers/DevelopersViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DevelopersViewController.swift
+//  Animal Shelter
+//
+//  Created by Leon Fridman on 9/9/22.
+//
+
+import UIKit
+
+class DevelopersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+}
