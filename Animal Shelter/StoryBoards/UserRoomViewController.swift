46b61557dbad4788a6276196d81b8f056815cb70
Pavel Shirokiy <pshirokiy@gmail.com>
2022-09-11 21:53:05 +0300

    new story

---
+++ Animal Shelter/StoryBoards/UserRoomViewController.swift
@@ -0,0 +1,34 @@
+//
+//  UserRoomViewController.swift
+//  Animal Shelter
+//
+//  Created by 19543442 on 11.09.2022.
+//
+
+import UIKit
+
+class UserRoomViewController: UIViewController {
+
+    @IBOutlet var userImage: UIImageView!
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var dogsOrderLabel: UILabel!
+    @IBOutlet var catsOrderLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func orderMore() {
+    }
+    
+
+    @IBAction func payAndExit() {
+    }
+    
+}
